28f7391eb4d0092cd5ff86224338f763f3698b3d
Aasen <aasen@scrymud.net>
1999-08-27 00:34:15 +0000

    ----------------------------------------------------------------------
      More tables and some cleaning.  Suppliment tables for pc's and crit
      are in the works.
    
     Committing in /ScryMUD/mud/grrmud/db
    
     Modified Files:
     	dbScry.sql
     ----------------------------------------------------------------------

--- mud/grrmud/db/dbScry.sql
+++ mud/grrmud/db/dbScry.sql
@@ -1,6 +1,6 @@
 # This is all being put together in MySql i will also
 # translate/change types so it will go easily into msql
-# shortly.  
+# when the main defs are finished.  
 #
 # If there is anything that needs to be changed please
 # feel free to dig in.  As for the missing stuff, it will
@@ -10,6 +10,103 @@
 #
 # Database: dbScry
 #--------------------------------------------------------
+# Server version	3.22.25
+
+#
+# Table structure for table 'tblBanList'
+#
+CREATE TABLE tblBanList (
+  BanID int(11) DEFAULT '0' NOT NULL,
+  Site char(20) DEFAULT '' NOT NULL,
+  Notes char(200) DEFAULT '' NOT NULL
+);
+
+#
+# Dumping data for table 'tblBanList'
+#
+
+
+#
+# Table structure for table 'tblCritter'
+#
+CREATE TABLE tblCritter (
+  CritID int(11) DEFAULT '0' NOT NULL,
+  Description text NOT NULL,
+  Name varchar(50) DEFAULT '' NOT NULL,
+  CritType int(11) DEFAULT '0' NOT NULL,
+  PRIMARY KEY (CritID)
+);
+
+#
+# Dumping data for table 'tblCritter'
+#
+
+
+#
+# Table structure for table 'tblMudStart'
+#
+CREATE TABLE tblMudStart (
+  ConfID int(11) DEFAULT '0' NOT NULL,
+  ConfName char(100) DEFAULT '' NOT NULL,
+  MaxObjNum int(11) DEFAULT '0' NOT NULL,
+  MaxDoorNum int(11) DEFAULT '0' NOT NULL,
+  MaxRoomNum int(11) DEFAULT '0' NOT NULL,
+  MaxCritNum int(11) DEFAULT '0' NOT NULL,
+  Year int(11) DEFAULT '0' NOT NULL,
+  Day int(11) DEFAULT '0' NOT NULL,
+  Hour int(11) DEFAULT '0' NOT NULL,
+  BootTime int(11) DEFAULT '0' NOT NULL,
+  DefaultPort int(11) DEFAULT '0' NOT NULL,
+  MaxDescriptorsAvail int(11) DEFAULT '0' NOT NULL,
+  MaxPlayers int(11) DEFAULT '0' NOT NULL,
+  BootLoadModifier int(11) DEFAULT '0' NOT NULL,
+  LoadModifier int(11) DEFAULT '0' NOT NULL,
+  RegularLoadModifier int(11) DEFAULT '0' NOT NULL,
+  OptUsec int(11) DEFAULT '0' NOT NULL,
+  FirstRoom int(11) DEFAULT '0' NOT NULL,
+  LastRoom int(11) DEFAULT '0' NOT NULL,
+  ROD timestamp(14),
+  LMD timestamp(14),
+  PRIMARY KEY (ConfID)
+);
+
+#
+# Dumping data for table 'tblMudStart'
+#
+
+
+#
+# Table structure for table 'tblPC'
+#
+CREATE TABLE tblPC (
+  PCID int(11) DEFAULT '0' NOT NULL,
+  Name char(50) DEFAULT '' NOT NULL,
+  password char(50) DEFAULT '' NOT NULL,
+  birthDay int(11) DEFAULT '0' NOT NULL,
+  birthYear int(11) DEFAULT '0' NOT NULL,
+  rentYear int(11) DEFAULT '0' NOT NULL,
+  rentDay int(11) DEFAULT '0' NOT NULL,
+  linePerPage int(11) DEFAULT '0' NOT NULL,
+  age int(11) DEFAULT '0' NOT NULL,
+  thirst int(11) DEFAULT '0' NOT NULL,
+  hunger int(11) DEFAULT '0' NOT NULL,
+  drugged int(11) DEFAULT '0' NOT NULL,
+  prompt int(11) DEFAULT '0' NOT NULL,
+  poofin char(255) DEFAULT '' NOT NULL,
+  poofout char(255) DEFAULT '' NOT NULL,
+  pkCount int(11) DEFAULT '0' NOT NULL,
+  diedCount int(11) DEFAULT '0' NOT NULL,
+  questPoints int(11) DEFAULT '0' NOT NULL,
+  lastLoginTime int(11) DEFAULT '0' NOT NULL,
+  canBeBeeped int(11) DEFAULT '0' NOT NULL,
+  totalTimeOnline int(11) DEFAULT '0' NOT NULL,
+  PRIMARY KEY (PCID)
+);
+
+#
+# Dumping data for table 'tblPC'
+#
+
 
 #
 # Table structure for table 'tblZFlags'
@@ -25,8 +122,7 @@ CREATE TABLE tblZFlags (
 );
 
 #
-# Some of the bit fields need to be changed i beleve to integer
-# to make them actual low bit high bit insted of boolean.
+# Dumping data for table 'tblZFlags'
 #
 
 
@@ -46,7 +142,7 @@ CREATE TABLE tblZone (
 );
 
 #
-# Did i miss any of the key elements?
+# Dumping data for table 'tblZone'
 #
 
 
@@ -60,8 +156,7 @@ CREATE TABLE tblxOwner (
 );
 
 #
-# Just a cross table for linking Owner/Player as Owners of a zone
-# This will be changed to reflect the ID field in the player tbl.
+# Dumping data for table 'tblxOwner'
 #
 
 
